ed447e7597e159547492f7548516ba081ee943a8
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-06 19:40:16 +0000

    create user migration

---
+++ db/migrate/20170406193750_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user do |t|
+      t.string :name
+      t.string :email
+      t.string :address
+      t.string :password_digest
+    end
+  end
+end
