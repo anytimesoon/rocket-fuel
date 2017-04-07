53be6ded92911bc3e55973fb921953ecb2ee7620
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-07 16:36:11 +0000

    moved form validation to helpers.rb

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,14 @@
 class Helpers
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
     !!session[:user_id]
   end
 
-  def current_user(session)
+  def self.current_user(session)
     User.find(session[:user_id])
+  end
+
+  def self.is_email?(email)
+    value = email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    !!value
   end
 end
