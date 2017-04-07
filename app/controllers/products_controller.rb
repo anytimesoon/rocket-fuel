0bf5d2892172d4f2f78138d382b9faeb3e85677e
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-07 19:39:08 +0000

    start of cart logic in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,22 @@ class ProductsController < Sinatra::Base
   enable :sessions unless test?
   set :session_secret, "secret"
 
-  get '/products/:slug'do
+  get '/products/:slug' do
     @product = Product.find_by(slug: params[:slug])
     erb :'products/show'
+  end
+
+  patch '/products/:slug' do
+    if !Helpers.is_logged_in?(session)
+      redirect '/login'
+    elsif !session[:cart_id]
+      user = current_user
+      product = Product.find_by(slug: params[:slug])
+      cart = Cart.create(product_id: product.id, user_id: user.id)
+      session[:cart_id] = cart.id
+    end
+
+    cart = Cart.find_by(session[:cart_id])
+    cart.update(params[:product_amount])
   end
 end
