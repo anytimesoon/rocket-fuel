6a8e659576d9170c0c21706db75a7bb1a28bef12
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-07 16:37:09 +0000

    sign up controller and form working

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,17 +1,33 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
   set :public_folder, './public'
   set :views, 'app/views'
   enable :sessions unless test?
   set :session_secret, "secret"
+  use Rack::Flash
 
   get '/signup' do
     erb :'signup/index'
   end
 
   post '/signup' do
-    binding.pry
+    if !Helpers.is_email?(params[:email])
+      flash[:message] = "Must be valid email address"
+      @params = params
+      redirect :'signup/index'
+    end
+
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect '/'
+    else
+      flash[:message] = "Something went wrong. Please try again."
+      @params = params
+      erb :'signup/index'
+    end
   end
 
 end
