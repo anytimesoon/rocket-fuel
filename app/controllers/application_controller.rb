8396655f6b3c1b9e3883ae2bcf745d5955fc4265
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-07 15:24:10 +0000

    add layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-  set :public_folder, 'public'
+  set :public_folder, './public'
   set :views, 'app/views'
   enable :sessions unless test?
   set :session_secret, "secret"
