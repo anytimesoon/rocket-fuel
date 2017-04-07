fbb445ebaf7ba90c0d00eb0a9a1c6bb5809ca65d
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-07 20:12:32 +0000

    untested cart logic in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 require './config/environment'
+require 'rack-flash'
 
 class ProductsController < Sinatra::Base
   set :public_folder, './public'
   set :views, 'app/views'
   enable :sessions unless test?
   set :session_secret, "secret"
+  use Rack::Flash
 
   get '/products/:slug' do
     @product = Product.find_by(slug: params[:slug])
@@ -12,16 +14,26 @@ class ProductsController < Sinatra::Base
   end
 
   patch '/products/:slug' do
+    user = current_user
+    product = Product.find_by(slug: params[:slug])
+
     if !Helpers.is_logged_in?(session)
       redirect '/login'
     elsif !session[:cart_id]
-      user = current_user
-      product = Product.find_by(slug: params[:slug])
       cart = Cart.create(product_id: product.id, user_id: user.id)
       session[:cart_id] = cart.id
     end
 
     cart = Cart.find_by(session[:cart_id])
-    cart.update(params[:product_amount])
+    cartline = Cartline.new(cart_id: cart.id, product_id: product.id, product_amount: params[:product_amount])
+
+    cart.cartlines << cartline
+    if cart.save
+      redirect '/cart'
+    else
+      flash[:message] = "Something didn't quite work. Please try again"
+      redirect "/products/#{product.slug}"
+    end
+
   end
 end
