bb45b9c0c2603ff026126d7f8ea1bb0e657f78b7
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-07 15:30:01 +0000

    add signup controller

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,13 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  set :public_folder, './public'
+  set :views, 'app/views'
+  enable :sessions unless test?
+  set :session_secret, "secret"
+
+  get '/signup' do
+    erb :'signup/index'
+  end
+
+end
