2fc3185cc66a32fbf57958a96ee37d2e090cca97
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-08 18:46:25 +0000

    add admin controller

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,11 @@
+require './config/environment'
+require 'rack-flash'
+
+class AdminController < Sinatra::Base
+  set :public_folder, './public'
+  set :views, 'app/views'
+  enable :sessions unless test?
+  set :session_secret, "secret"
+  use Rack::Flash
+
+end
