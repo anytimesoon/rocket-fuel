6b03e8e3fc6bcf713963fa6deaf150d75295e1c4
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-08 18:43:12 +0000

    create associations for admin class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :cartlines
   has_many :cart_ids, through: :cartlines
+  belongs_to :admin
   validates :name, presence: true
   validates :price, presence: true
 
